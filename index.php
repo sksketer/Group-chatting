40cfb998ddcc892b9c0f9aef5bf0dd1ec73d0a4b
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-29 15:37:07 +0530

    change name login to index

--- index.php
+++ index.php
@@ -4,30 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>sk chat</title>
+    <link rel="stylesheet" href="css/loginPage.css">
+    <title>Live Chat Login</title>
 </head>
 <body>
-    <div class="container">
-        <?php require 'components/navigationBar.php';?>
-        <div class="main">
-            <div class="displayMessage">
-                <div class="userMessage">
-                    <span id="user"><name>username</name></span>
-                    <span id="msg-container">
-                        <div id="msg">
-                            Hello what are you doing
-                        </div>
-                    </span>
+    <div class="main">
+        <form action="components/checkLogin.php" method="post">
+            <div class="dataField">
+                <div class="email">
+                    <p>Email</p>
+                    <input type="text" name="email" id="email" autofocus spellcheck="true">
+                </div>
+                <div class="password">
+                    <p>Password</p>
+                    <input type="password" name="password" id="password">
                 </div>
             </div>
-            <div class="textMessage">
-                <form action="" method="post">
-                    <input type="text" name="message" id="message" placeholder="message" autocomplete="off" spellcheck="true">
-                    <input type="submit" value="send" id="send-btn">
-                </form>
+            <div class="btn">
+                <input type="submit" value="Login" name="login" id="login-btn">
             </div>
-        </div>
+        </form>
     </div>
 </body>
 </html>
