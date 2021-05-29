f901a2906056528be1aa543951a55ee3c12372aa
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-29 17:32:34 +0530

    Update navigationBar.php

--- components/navigationBar.php
+++ components/navigationBar.php
@@ -11,7 +11,7 @@
 <body>
 <?php 
     echo '<div class="nav">';
-    echo '    <P id="logo"><img src="img/logo" alt="Logo Not Found" width="75px"></P>';
+    echo '    <P id="logo"><img src="img/logo.png" alt="Logo Not Found" width="75px"></P>';
     echo '    <p id="heading">Live Chatting</p>';
     echo '    <p id="login"><span id="username">'.$_SESSION['username'].'</span><a href="components/sessionOut.php"> Logout </a></p>';
     echo '</div>';
