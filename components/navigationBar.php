0acdb570c436a56f5c083a6b614586497831abc9
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-29 13:19:19 +0530

    Create navigationBar.php

---
+++ components/navigationBar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Nav-Bar</title>
+</head>
+<body>
+    <div class="nav">
+        <P id="logo"><img src="img/logo" alt="Logo Not Found" width="75px"></P>
+        <p id="heading">Live Chatting</p>
+        <p id="login"><a href="components/login.html"> Login </a></p>
+    </div>
+</body>
+</html>
