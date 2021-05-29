9941414dead079ceab1eeab551b9008ac4ab0923
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-29 15:37:22 +0530

    Update navigationBar.php

--- components/navigationBar.php
+++ components/navigationBar.php
@@ -1,17 +1,20 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Nav-Bar</title>
 </head>
 <body>
-    <div class="nav">
-        <P id="logo"><img src="img/logo" alt="Logo Not Found" width="75px"></P>
-        <p id="heading">Live Chatting</p>
-        <p id="login"><a href="components/login.html"> Login </a></p>
-    </div>
+<?php 
+    echo '<div class="nav">';
+    echo '    <P id="logo"><img src="img/logo" alt="Logo Not Found" width="75px"></P>';
+    echo '    <p id="heading">Live Chatting</p>';
+    echo '    <p id="login"><span id="username">'.$_SESSION['username'].'</span><a href="components/sessionOut.php"> Logout </a></p>';
+    echo '</div>';
+?>
 </body>
 </html>
