587fc9e3e82f0e02c2d542e759aabbf1f4354973
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-29 13:37:14 +0530

    Update checkLogin.php

--- components/checkLogin.php
+++ components/checkLogin.php
@@ -7,44 +7,43 @@
     $email = $_POST["email"]; 
     $password = $_POST["password"]; 
     
+    echo $email." ".$password;
     // Create connection
     $conn = mysqli_connect($servername, $username, $connection_password, $database);
+
+    // if($conn) echo "Connection sucessful";
     
-    $sql= "SELECT * FROM `userlogins` WHERE email = $email AND password = md5($password)";
+    $sql= "SELECT * FROM `userlogins`";
     $result=mysqli_query($conn,$sql);
 
     $checkemailis=$checkpasswordis=0;
     $username="";
     
-    if(isset($_POST["email"], $_POST["password"])) 
-    {
-        while($row=mysqli_fetch_assoc($result))
-        {
-            if($row['email'] == $email)
-            {
-                $checkemailis++;
-                if($password == md5($row['password']))
-                {
-                    $checkpasswordis++;
-                    $username=$row['username'];
-                    $user = $row['userType'];
-                }
+    
+    while($row=mysqli_fetch_assoc($result)) {
+        if($row['email'] == $email) {
+            $checkemailis++;
+            if(md5($password) == $row['password']) {
+                $checkpasswordis++;
+                $username=$row['username'];
+                $user = $row['userType'];
             }
         }
-
-        session_start();
-        $_SESSION['username'] = $username;
-        $_SESSION['email'] = $email;
-        $_SESSION['password'] = $password;
-
-        if($checkemailis > 0 && $checkpasswordis > 0) {
-            echo "You are normal user";
-            header("Location: ../index.php");
-        }
-        else if($checkemailis > 0 && $checkpasswordis == 0) {
-            echo "Password Not Matched";
-        } else {
-            echo "<div class='userNotFound'><p>user not found</p>"."<a href='signup.php'>create new account</a></div>";
-        }
     }
+
+    session_start();
+    $_SESSION['username'] = $username;
+    $_SESSION['email'] = $email;
+    $_SESSION['password'] = $password;
+
+    if($checkemailis > 0 && $checkpasswordis > 0) {
+        echo "You are normal user";
+        header("Location: ../index.php");
+    }
+    else if($checkemailis > 0 && $checkpasswordis == 0) {
+        echo "Password Not Matched";
+    } else {
+        echo "<div class='userNotFound'><p>user not found</p>"."<a href='signup.php'>create new account</a></div>";
+    }
+    
 ?>
