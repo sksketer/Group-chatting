cc0312b8fee06cc361930a078a9fdb89f9ff571e
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-06-01 18:05:21 +0530

    update main

--- main.php
+++ main.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>sk chat</title>
 </head>
 <body>
