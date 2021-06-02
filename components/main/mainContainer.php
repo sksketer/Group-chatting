31fa23d53855fcda22710ade469421774fb2b9d3
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-06-02 18:26:06 +0530

    update design

--- components/main/mainContainer.php
+++ components/main/mainContainer.php
@@ -37,35 +37,30 @@
         echo '<div class="activeUser">';
             echo '<p id="online">Online users</p>';
             while($usrrow = mysqli_fetch_assoc($usrresult)) {
-            echo '    <div id="user"></div>';
-            echo '    <p>'.$usrrow['username'].'</p>';
-            // echo '<p> username</p>';
+                echo '    <div id="user"></div>';
+                echo '    <p>'.$usrrow['username'].'</p>';
             }
         echo '</div>';
     ?>
     <?php
         echo '<div class="displayMessage">';
-            while($row = mysqli_fetch_assoc($msgresult)) {
-                // $flexDirection = ($_SESSION['username'] == $row['username']) ? "row-reverse" : "column";
+        while($row = mysqli_fetch_assoc($msgresult)) {
                 $color = ($_SESSION['username'] == $row['username']) ? "rgb(0, 90, 8)" : "rgb(32, 28, 28)";
                 $display = ($_SESSION['username'] == $row['username']) ? "visible" : "none";
-
-                echo '<div class="userMessage" style="">';
-                    echo '<span id="msg-container">';
-                        echo '<div id="msg">';
-                                echo '<form action="components/deleteMessage.php" method="post">';
-                                echo '    <input type="hidden" name="msgSno" value="'.$row['sno'].'" id="">';
-                                echo '    <input type="hidden" name="msgMessage" value="'.$row['message'].'" id="">';
-                                echo     $row['message'];
-                                // echo ' <i class="fa fa-trash" aria-hidden="true"></i>';
-                                echo '    <input type="submit" value="delete"  style="display: '.$display.'">';
-                                echo '</form>';
-                        echo '</div>';
-
-                        echo '<span id="user" style="Background-color: '.$color.';">';
-                                echo $row['username'];
-                        echo '</span>';
-                    echo '</span>';
+                $flexDirection = ($_SESSION['username'] == $row['username']) ? "row-reverse" : "row";
+                $margin = ($_SESSION['username'] == $row['username']) ? "" : "";
+                
+                echo '<div class="userMessage">';
+                    echo '<form action="components/deleteMessage.php" method="post" style="flex-direction:'.$flexDirection.'">';
+                        echo '<fieldset style="flex-direction:'.$flexDirection.'">';
+                            echo '<legend style="background-color: '.$color.'; margin-left: '.$margin.';">'.$row['username'].'</legend>';
+                            echo '<input type="hidden" name="msgSno" value="'.$row['sno'].'" id="">';
+                            echo '<input type="hidden" name="msgMessage" value="'.$row['message'].'" id="">';
+                            echo $row['message'];
+                            // echo ' <i class="fa fa-trash" aria-hidden="true"></i>';
+                            echo '<input type="submit" value="|_|"  style="display: '.$display.'" id="del-btn">';
+                        echo '</fieldset>';
+                    echo '</form>';
                     
                 echo '</div>';
             };
