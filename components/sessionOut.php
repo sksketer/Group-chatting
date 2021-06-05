cca71baf70472a0dd940e3a2455574a241fc765e
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-06-05 12:46:57 +0530

    change user status "Inactivr" to "offline"

--- components/sessionOut.php
+++ components/sessionOut.php
@@ -1,23 +1,22 @@
 <?php
-session_start();
+    session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <body>
+    <?php
+    
+    require 'connection/connectionFile.php';
+    $sno = $_SESSION['sno'];
+    //change the status of user from 'online' to 'offline'
+    mysqli_query($conn, "UPDATE `userlogins` SET `status` = 'offline' WHERE `userlogins`.`sno` = '$sno'");
+    
+    // remove all session variables
+    session_unset();
 
-<?php
-  
-$conn = mysqli_connect("localhost", "root", "", "live chatting");
-$sno = $_SESSION['sno'];
-//change the status of user from 'Active' to 'Inactive'
-mysqli_query($conn, "UPDATE `userlogins` SET `status` = 'Inactive' WHERE `userlogins`.`sno` = '$sno'");
-  
-// remove all session variables
-session_unset();
-
-// destroy the session
-session_destroy();
-header("Location: ../index.php");
-?>
+    // destroy the session
+    session_destroy();
+    header("Location: ../index.php");
+    ?>
 </body>
 </html>
