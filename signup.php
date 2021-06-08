2b5332d002281fb69ee384989254003d570a1fcf
sksketer <ksamrit121@gmail.com>
2021-06-08 13:45:02 +0530

    add password validation

--- signup.php
+++ signup.php
@@ -8,9 +8,6 @@
     <title>SignUP</title>
 </head>
 <body>
-    <?php
-        $username_err = $email_err = "";
-    ?>
     <div class="signup">
         <form action="components/submitRegestration.php" method="post">
             <p id="heading">SignUP for Group chat</p>
@@ -23,8 +20,11 @@
                 <input type="email" name="email" id="email" required> <span style="color: red;"> <?php echo $email_err; ?> </span> <br> <br>
             </div>
             <div class="password">
-                <label for="">Password</label> <br>
-                <input type="password" name="password" id="password" required> <br> <br>
+                <label for="psw">Password</label> <br>
+                <input type="password" name="password" id="password" 
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" 
+                required> <br> <br>
             </div>
             <div class="btn">
                 <input type="submit" value="Submit" name="submit" id="save-btn">
@@ -32,42 +32,58 @@
             </div>
             <a href="login.php">Alread have an Account?</a>
         </form>
+
     </div>
-    <!-- <?php
-        if(isset($_POST['submit'])) {
 
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $password = md5($_POST['password']);
-            
-            $conn = mysqli_connect("localhost", "root", "", "live chatting");
-            $msg = "SELECT username, email FROM `userlogins`";
-            $result= mysqli_query($conn, $msg);
-            $checkEmail = $checkUsername = 0;
-            while($row = mysqli_fetch_assoc($result) ) {
-                if($username == $row['username']) $checkUsername++;
-                if($email == $row['email']) $checkEmail++; 
-            }
-            echo "user";
-            echo $checkUsername." ".$checkEmail;
-            if($checkUsername == 0 && $checkEmail == 0) {
-                session_start();
-                $_SESSION['newUsername'] = $username;
-                $_SESSION['newEmail'] = $email;
-                $_SESSION['newPassword'] = $password;
-                echo "redirect";
-                // header("Location: components/submitRegestration.php");
-            } else {
-                echo "not redirect";
-                if($checkUsername > 0) $username_err = "UserName Alredy exist";
-                if($checkEmail > 0) $email_err = "Email already exist user another email";
-                echo $username_err." ".$email_err;
+    <script>
+        var myInput = document.getElementById("password");
+        var letter = document.getElementById("letter");
+        var capital = document.getElementById("capital");
+        var number = document.getElementById("number");
+        var length = document.getElementById("length");
 
-                echo $_SESSION['newUsername']."<br>";
-                echo $_SESSION['newEmail']."<br>";
-                echo $_SESSION['newPassword']."<br>";
-            }
+        // When the user starts to type something inside the password field
+        myInput.onkeyup = function() {
+        // Validate lowercase letters
+        var lowerCaseLetters = /[a-z]/g;
+        if(myInput.value.match(lowerCaseLetters)) {  
+            letter.classList.remove("invalid");
+            letter.classList.add("valid");
+        } else {
+            letter.classList.remove("valid");
+            letter.classList.add("invalid");
         }
-    ?> -->
+        
+        // Validate capital letters
+        var upperCaseLetters = /[A-Z]/g;
+        if(myInput.value.match(upperCaseLetters)) {  
+            capital.classList.remove("invalid");
+            capital.classList.add("valid");
+        } else {
+            capital.classList.remove("valid");
+            capital.classList.add("invalid");
+        }
+
+        // Validate numbers
+        var numbers = /[0-9]/g;
+        if(myInput.value.match(numbers)) {  
+            number.classList.remove("invalid");
+            number.classList.add("valid");
+        } else {
+            number.classList.remove("valid");
+            number.classList.add("invalid");
+        }
+        
+        // Validate length
+        if(myInput.value.length >= 8) {
+            length.classList.remove("invalid");
+            length.classList.add("valid");
+        } else {
+            length.classList.remove("valid");
+            length.classList.add("invalid");
+        }
+        }
+    </script>
+
 </body>
 </html>
