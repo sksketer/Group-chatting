ad475d8c06694ad382694f84ea91ebf9ee134fb7
sksketer <ksamrit121@gmail.com>
2021-06-08 12:33:40 +0530

    fix submitRegestration file

--- components/submitRegestration.php
+++ components/submitRegestration.php
@@ -6,17 +6,22 @@
     $password = md5($_POST['password']);
     $id = "";
 
+        echo $id."<br>";
+        echo $username."<br>";
+        echo $email."<br>";
+        echo $password."<br>";
 
     if($conn) {
-        $insertQuery = "INSERT INTO `userlogins` (`ID`, `username`, `email`, `password`) 
-                        VALUES ('$id', '$username', '$email', '$password')";
+        $insertQuery = "INSERT INTO `userlogins` (`sno`, `ID`, `username`, `email`, `password`, `status`) 
+                        VALUES
+                        (NULL, '', '$username', '$email', '$password', '');";
         
         $result= mysqli_query($conn, $insertQuery);
         if($result) {
             echo '<div style="color: lightgreen; text-align: center; font-weight: bolder; margin: 12px 0px;">';
-            echo "Regestration completed"."&nbsp;";
-            echo "You Need To sign-IN ";
-            echo '<a href="../login.php" style="color: skyblue;">Sign IN</a>';
+                echo "Regestration completed"."&nbsp;";
+                echo "You Need To sign-IN ";
+                echo '<a href="../login.php" style="color: skyblue;">Sign IN</a>';
             echo '</div>';
         } else
             echo "<div style='color:red; text-align: center; margin: 20px; font-size: larger;'>Error in Regestration. </div>";
