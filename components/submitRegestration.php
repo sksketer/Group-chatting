b37b41b4f69a1b2747f88274b0797d2a18804c1b
sksketer <ksamrit121@gmail.com>
2021-06-08 13:19:55 +0530

    at regestration time alreay used emailID can not be again used to more regestration

--- components/submitRegestration.php
+++ components/submitRegestration.php
@@ -5,24 +5,42 @@
     $email = $_POST['email'];
     $password = md5($_POST['password']);
     $id = "";
-
-    if($conn) {
-        $insertQuery = "INSERT INTO `userlogins` (`sno`, `ID`, `username`, `email`, `password`, `status`) 
-                        VALUES
-                        (NULL, '', '$username', '$email', '$password', '');";
+    $selectEmailQuery = "SELECT email FROM `userlogins`";
+    $emailResult = mysqli_query($conn, $selectEmailQuery);
+    if($emailResult) {
+        $count = 0;
+        while($count == 0 && $emailrow = mysqli_fetch_assoc($emailResult)) {
+            if($count ==0 && $emailrow['email'] == $email) {
+                echo '<script>
+                        alert("Email- '.$email.' is Already used by someone")
+                        window.location.href="../signup.php";                        
+                    </script>';
+                $count++;
+            } 
+        }
+        if($count > 0) {
+            echo "<div style='color:red; text-align: center; margin: 20px; font-size: larger; fint-weight: bold;'>Email is already used. </div>";
+        } else {
+            if($conn) {
+                $insertQuery = "INSERT INTO `userlogins` (`sno`, `ID`, `username`, `email`, `password`, `status`) 
+                                VALUES
+                                (NULL, '', '$username', '$email', '$password', '');";
+                
+                $result= mysqli_query($conn, $insertQuery);
+                if($result) {
+                    echo '<div style="color: lightgreen; text-align: center; font-weight: bolder; margin: 12px 0px;">';
+                        echo "Regestration completed"."&nbsp;";
+                        echo "You Need To sign-IN ";
+                        echo '<a href="../login.php" style="color: skyblue;">Sign IN</a>';
+                    echo '</div>';
+                } else
+                    echo "<div style='color:red; text-align: center; margin: 20px; font-size: larger;'>Error in Regestration. </div>";
         
-        $result= mysqli_query($conn, $insertQuery);
-        if($result) {
-            echo '<div style="color: lightgreen; text-align: center; font-weight: bolder; margin: 12px 0px;">';
-                echo "Regestration completed"."&nbsp;";
-                echo "You Need To sign-IN ";
-                echo '<a href="../login.php" style="color: skyblue;">Sign IN</a>';
-            echo '</div>';
-        } else
-            echo "<div style='color:red; text-align: center; margin: 20px; font-size: larger;'>Error in Regestration. </div>";
+            } else {
+                echo '<h1 style="color:red"> CantNot Connected to server. <a href="../signup.html">Try Again</a> </h1>';
+            }
+        }
 
-    } else {
-        echo '<h1 style="color:red"> CantNot Connected to server. <a href="../signup.html">Try Again</a> </h1>';
     }
 
 
